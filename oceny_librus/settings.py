31de06672b7c880dc55b78827a5f767be691c4ae
zamknijryj <zamknijryjx.x@tutanota.com>
2017-11-11 09:19:18 +0100

    Fixed some stuff with REST API

--- oceny_librus/settings.py
+++ oceny_librus/settings.py
@@ -115,6 +115,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
